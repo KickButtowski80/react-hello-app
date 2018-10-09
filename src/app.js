3462e1889950ee011c9ea8e91a9a0a99b82cbc65
KickButtowski80 <pazpaz25@gmail.com>
2018-10-08 18:10:55 -0700

    removing node_modules

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+ 
+const JSX = (
+  <div>
+    <h1>Hello world</h1>
+    <p>He attended a Cobb County High School.
+      Several news sources have reported Coyne is an Atlanta native. 
+      According to his Linkedin profile, he graduated from Alan C. 
+      Pope High School in East Cobb. He also graduated fro </p>
+  </div>
+
+);
+
+
+
+  ReactDOM.render( JSX , document.getElementById('app'));
+
+
+
+
+
+
+
+
